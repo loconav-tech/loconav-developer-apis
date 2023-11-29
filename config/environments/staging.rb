137253ecac12c13d931b9243cba14f5a7416e129
gaurav sharma <gauravsharma.mvp@gmail.com>
2023-11-29 17:47:01 +0530

    removed hosts

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
   # Enable server timing
   config.server_timing = true
 
-  config.hosts << "localhost"
+  # config.hosts << "localhost"
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
