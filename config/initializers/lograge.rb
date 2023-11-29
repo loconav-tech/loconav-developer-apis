37b66c818184df2a74cff46de6d189ed9e07f437
Raghav Bansal <raghav.bansal@loconav.com>
2023-11-29 18:10:37 +0530

    removed constants

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 FULL_LOG_HEADERS = Rails.application.secrets.full_log_headers
-FULL_LOG_RESOURCE_TYPE=Rails.application.secrets.full_log_resource_type
-FULL_LOG_SOURCE=Rails.application.secrets.full_log_source
+FULL_LOG_RESOURCE_TYPE = Rails.application.secrets.full_log_resource_type
+FULL_LOG_SOURCE = Rails.application.secrets.full_log_source
 
 Rails.application.configure do
   config.lograge.enabled = true
@@ -11,41 +11,35 @@ Rails.application.configure do
   config.lograge.custom_options = lambda do |event|
     {
       host: event.payload[:host],
-
       process_id: Process.pid,
       request_id: event.payload[:headers]["action_dispatch.request_id"],
-      LogConstants::REQUEST_START_TIME => Time.now.to_i,
-      LogConstants::MESSAGE => "full_log",
-      LogConstants::RESOURCE_TYPE => FULL_LOG_RESOURCE_TYPE,
-      LogConstants::SOURCE => FULL_LOG_SOURCE,
-      LogConstants::ELAPSED_TIME => event.duration.to_i.to_s + "ms".to_s,
-
+      :requestStartTime => Time.now.to_i,
+      :log_message => "full_log",
+      :resourceType => FULL_LOG_RESOURCE_TYPE,
+      :source => FULL_LOG_SOURCE,
+      :elapsedTime => event.duration.to_i.to_s + "ms".to_s,
       remote_ip: event.payload[:remote_ip],
       ip: event.payload[:ip],
       x_forwarded_for: event.payload[:x_forwarded_for],
-
       params: event.payload[:params].to_json,
       exception: event.payload[:exception]&.first,
       exception_message: event.payload[:exception]&.last.to_s,
       # exception_backtrace: event.payload[:exception_object]&.backtrace&.join(","),
       time: event.time,
-
     }
   end
   config.lograge.custom_payload do |controller|
     {
       host: controller.request.host,
-      LogConstants::RESPONSE_PAYLOAD => controller.response.body.as_json,
-      LogConstants::REQUEST_PAYLOAD => controller.request.body.read.to_s,
-      LogConstants::HTTP_METHOD => controller.request.request_method.to_s,
-      LogConstants::RESOURCE_URI => controller.request.fullpath.to_s,
-      LogConstants::REQUEST_PARAM => controller.params.as_json,
-      LogConstants::MDC_USER_ID => controller.current_account["id"].to_s,
-      LogConstants::RESPONSE_CODE => controller.response.status.to_s,
-      LogConstants::RESOURCE_URL => controller.request.url.to_s,
-
-      LogConstants::HEADERS => get_desired_headers(controller),
-
+      :responsePayload => controller.response.body.as_json,
+      :requestPayload => controller.request.body.read.to_s,
+      :httpMethod => controller.request.request_method.to_s,
+      :resourceURI => controller.request.fullpath.to_s,
+      :requestParam => controller.params.as_json,
+      :"User-Id" => controller.current_account["id"].to_s,
+      :responseCode => controller.response.status.to_s,
+      :resourceURL => controller.request.url.to_s,
+      :headers => get_desired_headers(controller),
 
     }
   end
