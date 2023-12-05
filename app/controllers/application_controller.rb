fe3cc0a61574471ad230a14d3358817fa1a7e6bc
gaurav sharma <gauravsharma.mvp@gmail.com>
2023-12-05 11:14:20 +0530

    added 404 handler:

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
   rescue_from StandardError, with: :handle_500
+  rescue_from ActionController::RoutingError, with: :handle_404
 
   private
 
@@ -11,5 +12,15 @@ class ApplicationController < ActionController::API
                                                              code: :internal_server_error,
                                                            }])
     render json: response, status: :internal_server_error
+  end
+
+  def handle_404(exception)
+    Rails.logger.error(exception)
+    Rails.logger.error(exception.backtrace.join("\n"))
+    response = Loconav::Response::Builder.failure(errors: [{
+                                                             message: "No API available",
+                                                             code: :not_found,
+                                                           }])
+    render json: response, status: :not_found
   end
 end
