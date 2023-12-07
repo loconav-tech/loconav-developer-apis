915534875f1113c5b129eb5e16b7c22be45bbcfd
Raghav Bansal <raghav.bansal@loconav.com>
2023-12-07 13:59:45 +0530

    removed unused method handle_errors in vt_data_service.rb

--- app/services/vehicle/telematics/vt_data_service.rb
+++ app/services/vehicle/telematics/vt_data_service.rb
@@ -5,7 +5,7 @@ module Vehicle
 
       include UtilHelper, ResponseHelper, VtHelper
 
-      attr_accessor :auth_token, :status_code, :errors,
+      attr_accessor :auth_token, :status_code, :errors
 
         def initialize
           self.errors = []
@@ -15,18 +15,6 @@ module Vehicle
         vt_lookups
       end
 
-      private def handle_errors(error_response)
-        case error_response
-        when /not supported/
-          errors << error_response
-        when "Data not found"
-          errors << "Data not found"
-          self.error_code = :data_not_found
-        else
-          errors << "Technical issue, please try again later"
-          self.error_code = :technical_issue
-        end
-      end
     end
   end
 end
