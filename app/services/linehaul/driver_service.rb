93ef20274d7186b52230b1e2ded85206f95d22c4
nishantbansal1910 <nishant.bansal@loconav.com>
2024-01-04 18:11:35 +0530

    create driver api

--- app/services/linehaul/driver_service.rb
+++ app/services/linehaul/driver_service.rb
@@ -4,11 +4,13 @@ module Linehaul
 
     include ResponseHelper
 
+    V5_API_ACCESS_TOKEN = Rails.application.secrets.v5_api_access_token
     CONNECTION_TIMEOUT = 20
     TIMEOUT = 20
     LINEHAUL_BASE_URL = Rails.application.secrets.linehaul_base_url
     FETCH_DRIVER_URL = LINEHAUL_BASE_URL + "/api/v5/drivers"
     ERROR_MSG_DRIVER = "Error fetching data for driver".freeze
+    DRIVER_URL = LINEHAUL_BASE_URL + "/api/v5/partner/drivers"
 
     attr_accessor :auth_token
 
@@ -29,5 +31,48 @@ module Linehaul
       ).run
       respond(response, ActionFailed, ERROR_MSG_DRIVER)
     end
+
+    def create_drivers(params)
+      response = Typhoeus::Request.new(
+        DRIVER_URL + "/onboard",
+        headers: {
+          Authorization: auth_token,
+          "X-Linehaul-V5-Secret": V5_API_ACCESS_TOKEN,
+          "User-Type": "User",
+          "Content-Type": "application/json"
+        },
+        body: build_request_body(params).to_json,
+        timeout: TIMEOUT,
+        connecttimeout: CONNECTION_TIMEOUT,
+        method: :post,
+        ).run
+      parse_response(response)
+    end
+
+    def build_request_body(params)
+      {
+        "drivers": params[:drivers]
+      }.compact
+    end
+
+    private def parse_response(response)
+      if response && response.body.present?
+        if response.success?
+          response_data = JSON.parse(response.body)
+          [true, response_data]
+        elsif response.response_code == 400
+          response_data = JSON.parse(response.body)
+          [400, response_data&.dig("error")]
+        elsif response.response_code == 422
+          response_data = JSON.parse(response.body)
+          [422, response_data&.dig("message","error")]
+        else
+          [false, "Technical issue"]
+        end
+      else
+        [false, "Technical issue"]
+      end
+    end
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :drivers, only: [:index]
+      resources :drivers, only: [:index,:create]
 
       resources :throttler, only: [:index, :create] do
         collection do
